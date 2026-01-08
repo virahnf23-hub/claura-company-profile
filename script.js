0b54ed2b37b191c11fb24e315dbdffe3940b0fe6
virahnf23-hub <virahnf23@gmail.com>
2026-01-08 11:26:13 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const greeting = document.getElementById("greeting");
-const hour = new Date().getHours();
-
-if (hour >= 5 && hour < 12) greeting.innerText = "Selamat Pagi 🌤️";
-else if (hour >= 12 && hour < 15) greeting.innerText = "Selamat Siang ☀️";
-else if (hour >= 15 && hour < 18) greeting.innerText = "Selamat Sore 🌅";
-else greeting.innerText = "Selamat Malam 🌙";
