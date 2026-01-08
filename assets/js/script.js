9b778c4f9bc14f88b8cd94df5d2ed0440755538c
virahnf23-hub <virahnf23@gmail.com>
2026-01-08 11:16:44 +0700

    Implement dynamic greeting based on time of day
    Add dynamic greeting based on the current hour.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+const greetingText = document.getElementById("greeting");
+
+const hour = new Date().getHours();
+let greeting = "";
+
+if (hour >= 5 && hour < 12) {
+  greeting = "Selamat Pagi";
+} else if (hour >= 12 && hour < 15) {
+  greeting = "Selamat Siang";
+} else if (hour >= 15 && hour < 18) {
+  greeting = "Selamat Sore";
+} else {
+  greeting = "Selamat Malam";
+}
+
+if (greetingText) {
+  greetingText.innerHTML = greeting + ", Selamat Datang di CLAURA";
+}
 
